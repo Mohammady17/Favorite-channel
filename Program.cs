49c20f7f1a1d1f3a39c7517d3f3400f28061ef9d
Mohammad <mohammad.yi2020@gmail.com>
2026-02-21 00:03:48 +0330

    Get into git

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int channelNumber = 0;
+            string userOpt;
+            do
+            {
+                channelNumber++;
+                Console.WriteLine($"**Channel {channelNumber}**");
+                Console.WriteLine("Do you love this channel? (yes/no)");
+                userOpt = Console.ReadLine().ToUpper();
+
+            } while (userOpt == "NO");
+            Console.WriteLine($"Enjoy watching channel {channelNumber}");
+            Environment.Exit(0);
+        }
+    }
+}
